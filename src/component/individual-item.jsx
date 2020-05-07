12e07366db1c81b91ee098da8f6ce4f2bb48b785
aalaprawat <aalaprawatdurg@gmail.com>
2020-05-07 18:40:27 +0530

    Sass files

---
+++ src/component/individual-item.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './individual-item.scss'
+
+const Individualitem = ({id,title,discount}) =>{
+    return (
+        <div className ="IndividualItems">
+            <div className="back" style ={{backgroundImage:`url(${`https://i.picsum.photos/id/${861+id}/1000/1000.jpg`}`}}>
+            </div>
+            <div className="Title">
+                    {title+"      "}
+                    {discount + id - Math.floor(Math.random()*20)}%
+            </div>
+            
+        </div>
+    )
+}
+
+export default Individualitem
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Directory from '../component/Directory'
+import Header from '../component/Header'
+import './homepage.scss'
+const Homepage =()=>{
+    return (
+    <div class="MainPage">
+        <Header/>
+            <Directory/>
+      
+     </div>    
+    )
+}
+
+export default Homepage
