0188f4dec37cce5e7df3957241bfa0e83438c70b
aalaprawat <aalaprawatdurg@gmail.com>
2020-05-07 18:39:52 +0530

    Updates

---
+++ precache-manifest.188d309c41dc9a795ca153f89a68b166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a63d3d14278f6a72325739050655e9",
+    "url": "/tumblr/index.html"
+  },
+  {
+    "revision": "9a0d8855e75ce39fea83",
+    "url": "/tumblr/static/css/main.9ab0f472.chunk.css"
+  },
+  {
+    "revision": "6e62e9ba9facc9ff31b0",
+    "url": "/tumblr/static/js/2.cbb95e4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tumblr/static/js/2.cbb95e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0d8855e75ce39fea83",
+    "url": "/tumblr/static/js/main.e292c53f.chunk.js"
+  },
+  {
+    "revision": "54a68503d9501ff4ba5e",
+    "url": "/tumblr/static/js/runtime-main.ffb20d92.js"
+  }
+]);
